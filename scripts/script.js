1e03677315b5c4954f8320cc1628bfa19ea4f076
Abin Thaha <abin@qburst.com>
2015-10-20 18:42:28 +0530

    Made the page responsive in all views

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+function toggleView(e) {
+    var x = document.getElementById('main_news_letter');
+    x.className = "news_letter hide_noti";
+    var y = document.getElementById('hidden_div');
+    y.className = "news_letter show_noti";
+    console.log(x);
+}
+function show_hidden_menu() {
+    var hidden_menu = document.getElementById('hidden_header');
+    if (hidden_menu.className == "hidden_header show") {
+        hidden_menu.className = "hidden_header hide";
+    }
+    else {
+        hidden_menu.className = "hidden_header show";
+    }
+}
